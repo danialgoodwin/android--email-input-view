98106ab2d5ebd9319784b4b552e660f1031d2afa
Danial Goodwin <danialgoodwin@gmail.com>
2015-12-28 18:11:14 -0500

    Add error message for invalid emails

--- email-input-view/lib-emailinputview/src/main/java/com/danialgoodwin/ui/emailinputview/EmailInputView.java
+++ email-input-view/lib-emailinputview/src/main/java/com/danialgoodwin/ui/emailinputview/EmailInputView.java
@@ -5,10 +5,14 @@ package com.danialgoodwin.ui.emailinputview;
 
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.graphics.Rect;
 import android.os.Build;
 import android.text.InputType;
+import android.text.TextUtils;
 import android.util.AttributeSet;
+import android.util.Patterns;
 import android.widget.EditText;
+import android.widget.TextView;
 
 /** An enhanced EditText with easy ability to get email from user. */
 public class EmailInputView extends EditText {
@@ -34,9 +38,34 @@ public class EmailInputView extends EditText {
         initialize(context);
     }
 
+    @Override
+    protected void onFocusChanged(boolean focused, int direction, Rect previouslyFocusedRect) {
+        super.onFocusChanged(focused, direction, previouslyFocusedRect);
+        if (!focused) { validateEmail(); }
+    }
+
+    @Override
+    public void onEditorAction(int actionCode) {
+        super.onEditorAction(actionCode);
+        validateEmail();
+    }
+
+    public static boolean isValidEmail(CharSequence email) {
+        return !TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
     private void initialize(Context context) {
         setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
         setHint(R.string.emailinputview_hint);
     }
 
+    protected void showErrorMessage() {
+        setError(getContext().getString(R.string.emailinputview_invalid_error));
+    }
+
+    private void validateEmail() {
+        if (getText().length() != 0 && !isValidEmail(getText())) {
+            showErrorMessage();
+        }
+    }
 }
