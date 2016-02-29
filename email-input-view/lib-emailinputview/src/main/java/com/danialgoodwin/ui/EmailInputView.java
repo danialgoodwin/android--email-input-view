2c3216bc19f4ae43abde752530474537267f4375
Satya Chaitanya <satya.chaitanya@zimmber.com>
2016-03-01 01:01:05 +0530

    Replace space character with empty string

--- email-input-view/lib-emailinputview/src/main/java/com/danialgoodwin/ui/EmailInputView.java
+++ email-input-view/lib-emailinputview/src/main/java/com/danialgoodwin/ui/EmailInputView.java
@@ -7,8 +7,11 @@ import android.annotation.TargetApi;
 import android.content.Context;
 import android.graphics.Rect;
 import android.os.Build;
+import android.text.Editable;
 import android.text.InputType;
+import android.text.SpannableStringBuilder;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.util.AttributeSet;
 import android.util.Patterns;
 import android.widget.EditText;
@@ -18,6 +21,7 @@ import com.danialgoodwin.ui.emailinputview.R;
 /** An enhanced EditText with easy ability to get valid email from user. An error message will
  * appear for invalid emails. */
 public class EmailInputView extends EditText {
+    private static final String TAG = "EmailInputView";
 
     public EmailInputView(Context context) {
         super(context);
@@ -63,6 +67,27 @@ public class EmailInputView extends EditText {
     private void initialize() {
         setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
         setHint(R.string.emailinputview_hint);
+        addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                SpannableStringBuilder stringBuilder = (SpannableStringBuilder) s;
+                for (int i = 0; i < count; i++) {
+                    int currentIndex = start + i;
+                    if (Character.isWhitespace(stringBuilder.charAt(currentIndex))) {
+                        // replacing space with empty string
+                        stringBuilder.replace(currentIndex, currentIndex + 1, "");
+                    }
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
     }
 
     protected void showErrorMessage() {
